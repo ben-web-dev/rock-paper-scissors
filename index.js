32a734dfff6339b8ccaafee6cf246c47db1ca2bd
ben-web-dev <mrbenrice@yahoo.com>
2024-03-24 16:24:25 +0000

    Created a basic game in Javascript file - Colour palette added to CSS for easy reference - Uses a random number to generate a phrase for the computer's turn - Every time a button is clicked, it plays a turn - When either the player or the computer reaches a score of 5, an   array is updated with the game number, the player's score, and the   computer's score. This will be edited in the future to be displayed   as a local leaderboard. - Visual updates will be added in later commits.

--- index.js
+++ index.js
@@ -1,13 +1,63 @@
+ const container = document.querySelector('.container');
  const rock = document.getElementById("rock");
  const paper = document.getElementById("paper");
  const scissors = document.getElementById("scissors");
+ const footer = document.querySelector(".footer");
 
  const buttons = [rock, paper, scissors];
+ let record = []
+ let gameCount = [];
+ let playerScore = 0;
+ let comperScore = 0;
+ let gameTracker = [];
 
  buttons.forEach(button => {
     button.addEventListener('click', () => {
-        console.log(button.getAttribute("id"));
+        const buttonSelected = button.getAttribute("id");
+        playTurn(buttonSelected);
     })
  })
 
+function playTurn(choice) {
+    let aiTurn = computerChoice();
+    if (choice === 'rock' && aiTurn === 'scissors' ||
+        choice === 'paper' && aiTurn === 'rock'    ||
+        choice === 'scissors' && aiTurn === 'paper'  ) {
+            console.log('you win!')
+            playerScore++
+    } else if
+       (choice === 'rock' && aiTurn === 'paper'    ||
+        choice === 'paper' && aiTurn === 'scissors'||
+        choice === 'scissors' && aiTurn === 'rock'   ) {
+            console.log('you lose!')
+            comperScore++
+    } else {
+            console.log('draw!');
+    }
+    if (playerScore === 5 || comperScore === 5) {
+        endCurrentGame();
+    }
+}
 
+function endCurrentGame() {
+    gameCount++
+    gameTracker.push({
+        'Game': gameCount,
+        'Player Score:': playerScore,
+        'Computer Score': comperScore
+    })
+    console.log(gameTracker);
+    playerScore = 0;
+    comperScore = 0;
+}
+
+function computerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+    if (choice === 0) {
+        return 'rock';
+    } else if (choice === 1) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
+}
