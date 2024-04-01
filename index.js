7855f5624ca9a5003a75a4d096154680a4594b67
ben-web-dev <mrbenrice@yahoo.com>
2024-04-01 20:19:01 +0100

    Added an interface - On top of the buttons, now added a 'game history' button that allows   the player to see a history of the games between themselves and the   computer. Local storage capabilities will be added at a later date. - A rules div has been added to the header: this will eventually show   a text box explaining the rules of rock, paper, scissors. - A div showing the player & computer's score for that game has been   added - A div showing the outcome of the game has been added - A div showing the outcome of each turn has been added - Various CSS changes including making the buttons bigger and centering   the game board. Colours will more than likely be changed in further   commits

--- index.js
+++ index.js
@@ -3,6 +3,11 @@
  const paper = document.getElementById("paper");
  const scissors = document.getElementById("scissors");
  const footer = document.querySelector(".footer");
+ const turnDiv = document.querySelector('.turn-announcement');
+ const score = document.querySelector('.score');
+ const gameDiv = document.querySelector('.game-announcement');
+ const histBtn = document.querySelector('.history-button');
+ const history = document.querySelector('.history');
 
  const buttons = [rock, paper, scissors];
  let record = []
@@ -10,6 +15,7 @@
  let playerScore = 0;
  let comperScore = 0;
  let gameTracker = [];
+
 
  buttons.forEach(button => {
     button.addEventListener('click', () => {
@@ -20,31 +26,43 @@
 
 function playTurn(choice) {
     let aiTurn = computerChoice();
+    gameDiv.textContent = '';
     if (choice === 'rock' && aiTurn === 'scissors' ||
         choice === 'paper' && aiTurn === 'rock'    ||
         choice === 'scissors' && aiTurn === 'paper'  ) {
-            console.log('you win!')
-            playerScore++
+            console.log('you win!');
+            turnDiv.textContent = 'You win!';
+            ++playerScore;
     } else if
        (choice === 'rock' && aiTurn === 'paper'    ||
         choice === 'paper' && aiTurn === 'scissors'||
         choice === 'scissors' && aiTurn === 'rock'   ) {
-            console.log('you lose!')
-            comperScore++
+            console.log('you lose!');
+            turnDiv.textContent = 'You lose!';
+            ++comperScore;
     } else {
             console.log('draw!');
+            turnDiv.textContent = 'Draw!';
     }
+    score.textContent = `Player ${playerScore} : ${comperScore} Computer`
     if (playerScore === 5 || comperScore === 5) {
         endCurrentGame();
     }
 }
 
 function endCurrentGame() {
+    if (playerScore === 5) {
+        gameDiv.textContent = `You win this game ${playerScore}:${comperScore}`;
+    } else if (comperScore === 5) {
+        gameDiv.textContent = `Computer wins this game ${comperScore}:${playerScore}`;
+    }
+    turnDiv.textContent = 'Choose to play again!';
+    score.textContent = '';
     gameCount++
     gameTracker.push({
-        'Game': gameCount,
-        'Player Score:': playerScore,
-        'Computer Score': comperScore
+        game: gameCount,
+        pscore: playerScore,
+        cscore: comperScore
     })
     console.log(gameTracker);
     playerScore = 0;
@@ -61,3 +79,31 @@ function computerChoice() {
         return 'scissors';
     }
 }
+
+histBtn.addEventListener('click', () => {
+    history.innerHTML = '';
+    history.style.visibility = 'visible';
+
+    const historyExit = document.createElement('button');
+    historyExit.innerHTML = 'X';
+    historyExit.style.right = '0';
+    historyExit.addEventListener('click', () => {
+        history.style.visibility = 'hidden';
+    })
+    history.appendChild(historyExit);
+    gameTracker.forEach(game => {
+        const gameTrackerDiv = document.createElement('div');
+        gameTrackerDiv.classList.add('game-tracker-div');
+        const gameTrackerDivGame = document.createElement('p');
+        const gameTrackerDivPlayer = document.createElement('p');
+        const gameTrackerDivComper = document.createElement('p');
+        gameTrackerDivGame.textContent = `Game: ${game.game}`;
+        gameTrackerDivPlayer.textContent = `Player: ${game.pscore}`;
+        gameTrackerDivComper.textContent = `Computer: ${game.cscore}`;
+        gameTrackerDiv.appendChild(gameTrackerDivGame);
+        gameTrackerDiv.appendChild(gameTrackerDivPlayer);
+        gameTrackerDiv.appendChild(gameTrackerDivComper);
+
+        history.appendChild(gameTrackerDiv);
+    });
+})
